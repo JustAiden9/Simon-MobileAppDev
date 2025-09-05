0a3a4e99aa9d05a25ffdc100e5cfc175756ba893
Aiden <aidenbak9@gmail.com>
2025-09-05 08:05:49 -0500

    Initial UI: black background, simon text

--- Simon/ContentView.swift
+++ Simon/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            
+            Text("Simon")
+                .font(.system(size: 72, weight: .bold))
+                .foregroundColor(.white)
         }
-        .padding()
+        .preferredColorScheme(.dark)
     }
 }
 
